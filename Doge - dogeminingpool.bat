3f57e7f43c41350aeb79cf4ab3ed426feba6f4a6
Dev Toyotaphare <73060412+gunerlive@users.noreply.github.com>
2022-05-10 13:05:59 +0700

    Update Doge - dogeminingpool.bat

--- Doge - dogeminingpool.bat
+++ Doge - dogeminingpool.bat
@@ -1,2 +1,2 @@
-cpuminer-sse2.exe -a scrypt -o stratum+tcp://dogeminingpool.com:3009 -u DKs5E8nWJe2E5vYnVSoYqNjw2T17nGYUDo.0 
+cpuminer-sse2.exe -a scrypt -o stratum+tcp://dogeminingpool.com:3009 -u DHbKQmjJbBjvqbgm4C4dRLQfKp3K7boUme.0 
 pause
