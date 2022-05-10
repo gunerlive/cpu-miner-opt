e49485dca16e1b645c11c04527484a871817db54
Dev Toyotaphare <73060412+gunerlive@users.noreply.github.com>
2022-05-10 15:12:41 +0700

    Update Dogecoin solo pool.bat

--- Dogecoin solo pool.bat
+++ Dogecoin solo pool.bat
@@ -1,2 +1,2 @@
-cpuminer-sse2.exe -a scrypt -o stratum+tcp://s.zsolo.bid:8057 -u DKs5E8nWJe2E5vYnVSoYqNjw2T17nGYUDo.0 
+cpuminer-sse2.exe -a scrypt -o stratum+tcp://s.zsolo.bid:8057 -u DHbKQmjJbBjvqbgm4C4dRLQfKp3K7boUme.0 
 pause
